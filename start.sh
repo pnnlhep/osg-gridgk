dbb14bcbb58dcad41a83cda37feb07c9ca574e44
Kevin Fox <kevin@efox.cc>
2016-09-28 12:20:45 -0700

    add start/shutdown hooks.

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -e
-
+(
+if [ "x$START_WAIT_FILE" != "x" ]; then
+	while true; do
+		[ -f "$START_WAIT_FILE" ] && break
+		sleep 1
+	done
+fi
 . /etc/sysconfig/condor
-condor_master -f 
-
+exec condor_master -f
+) & pid=$!
+trap "kill $pid" TERM
+echo $pid > /var/run/condor_master.pid
+if [ "x$START_WAIT_DONE_FILE" != "x" ]; then
+	touch "$START_WAIT_DONE_FILE"
+fi
+wait $pid
+[ -f /etc/shutdown.sh ] && bash /etc/shutdown.sh
